2dbcf5283fe93bfe4961a88b66002c7735db7b4b
20Jimenez <eduarjimenez6201@gmail.com>
2024-10-25 12:23:45 -0500

    Taller4

--- OBJETOS/HERENCIA/ENCAPSULACION/TALLER/sistemaCursos2.js
+++ OBJETOS/HERENCIA/ENCAPSULACION/TALLER/sistemaCursos2.js
@@ -4,10 +4,17 @@ class Curso{
         this.estudiantes = [];
     }
     agragarEstudiantes(estudiante){
-        this.estudiantes.push(estudiante)
+        this.estudiantes.push(estudiante);
     }
     calcularPromedioCurso(){
-        this.estudiantes
+        let suma = 0;
+        for(let i = 0; i < this.estudiantes.length; i++){
+            for(let j = 0;  j < this.estudiantes[i].calificaciones.length; j++){
+                suma +=  this.estudiantes(i).calificaciones(j);
+                total = total + 1;
+            }
+        }
+        return suma / total;
     }
 }
 
@@ -29,7 +36,7 @@ class Estudiante extends Persona {
         super(nombre, edad);
         this.calificaciones = [];
     }
-    agregarCalificaion(calificacion){ //calificacion debe ser un valor numerico
+    agregarCalificacion(calificacion){ //calificacion debe ser un valor numerico
         if(calificacion){
             this.calificaciones.push( parseFloat(calificacion) )
         }else{
@@ -53,9 +60,13 @@ const perso1 = new Persona("Ramon Valdez", 58);
 console.log(perso1.saludar());
 
 const estu1 = new Estudiante("Sebastian Rengifo", 25);
+const estu2 = new Estudiante("Camilo Perez", 25);
 console.log( estu1.saludar() );
-estu1.agregarCalificaion(2.3);
-estu1.agregarCalificaion(1.5);
-estu1.agregarCalificaion(4.0);
+estu1.agregarCalificacion(30);
+estu2.agregarCalificacion(80);
+let prog = new Curso("Programacion");
 
-console.log( estu1.calcularPromedio() )
+//console.log( estu1.calcularPromedio() )
+prog.agragarEstudiantes(estu1);
+prog.agragarEstudiantes(estu2);
+console.log( prog.calcularPromedioCurso());
